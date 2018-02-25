bb9b61f80037c68b1ffed01fcee329f59561df62
Marcel Köpke <marcel.koepke@gmail.com>
2018-02-25 18:43:22 +0100

    dynamic command-t path

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-COMMANDTPATH="$HOME/.vim/bundle/command-t-5.0.2/ruby/command-t/ext/command-t"
-
-
 echo "----------------------------------------"
 echo " start: setup of command-t"
 echo "----------------------------------------"
-cd $COMMANDTPATH
+COMMANDTPATH=`ls $HOME/.vim/bundle/ | grep command-t`
+COMMANDTBUILDPATH="$HOME/.vim/bundle/command-t-5.0.2/ruby/command-t/ext/command-t"
+
+cd $COMMANDTBUILDPATH
 ruby extconf.rb
 make
 echo "----------------------------------------"
