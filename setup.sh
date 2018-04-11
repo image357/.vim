7fe7ab17712eb91db0b41e5dbe0c8c502a15785a
Marcel Köpke <marcel.koepke@gmail.com>
2018-04-11 16:40:53 +0200

    cleanup of setup

--- setup.sh
+++ setup.sh
@@ -1,33 +1,63 @@
 #!/bin/bash
 set -e
-cd "$HOME/.vim"
+
+# path variables
+BASE_PATH="$HOME/.vim"
+ACKVIM_PATH="$BASE_PATH/bundle/ack.vim"
+YOUCOMPLETEME_PATH="$BASE_PATH/bundle/YouCompleteMe"
+
+
+# print functions
+function startsetup {
+    echo ""
+    echo "----------------------------------------"
+    echo " start: setup of $1"
+    echo "----------------------------------------"
+    echo ""
+}
+
+function endsetup {
+    echo ""
+    echo "----------------------------------------"
+    echo " done: setup of $1"
+    echo "----------------------------------------"
+    echo ""
+}
+
 
 # init submodules
+cd "$BASE_PATH"
 git submodule update --init
 git submodule update --recursive --remote
 
 
-echo ""
-echo "----------------------------------------"
-echo " start: setup of command-t"
-echo "----------------------------------------"
+# setup ack.vim
+startsetup ack.vim
+cd "$ACKVIM_PATH"
+git checkout tags/1.0.9
+endsetup ack.vim
+
+
+# setup command-t
+startsetup "command-t"
 COMMANDTPATH=`ls "$HOME/.vim/bundle/" | grep command-t`
 COMMANDTBUILDPATH="$HOME/.vim/bundle/$COMMANDTPATH/ruby/command-t/ext/command-t"
 
 cd "$COMMANDTBUILDPATH"
 ruby extconf.rb
 make
-echo "----------------------------------------"
-echo " done: setup of command-t"
-echo "----------------------------------------"
+endsetup "command-t"
 
 
-echo ""
-echo "----------------------------------------"
-echo " start: setup of vim Helptags"
-echo "----------------------------------------"
+# setup YouCompleteMe
+startsetup "YouCompleteMe"
+cd "$YOUCOMPLETEME_PATH"
+git checkout master
+endsetup "YouCompleteMe"
+
+
+# init Helptags
+startsetup "vim Helptags"
 vim +Helptags +qall
-echo "----------------------------------------"
-echo " done: setup of vim Helptags"
-echo "----------------------------------------"
+endsetup "vim Helptags"
 
