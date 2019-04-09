40243f0315abf34194e184ff69f61cc0941effd5
Marcel Köpke <marcel.koepke@gmail.com>
2019-04-09 17:19:57 +0200

    ycm extra conf python support

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,15 +1,34 @@
-default_flags = [
-        "-x",
-        "c++",
-        "-std=c++17",
-        "-stdlib=libc++",
-        "-Wall",
-        "-Wextra",
-]
+def get_cpp_flags():
+    flags = [
+            "-x",
+            "c++",
+            "-std=c++17",
+            "-stdlib=libc++",
+            "-Wall",
+            "-Wextra",
+    ]
+
+    return flags
+
+
+def get_python_sys_path():
+    sys_path = []
+
+    return sys_path
 
 
 def Settings(**kwargs):
-    return {
-            "flags":
-                    default_flags
-    }
+    language = kwargs["language"]
+
+    if language == "cfamily":
+        return {
+                "flags": get_cpp_flags(),
+        }
+
+    if language == "python":
+        return {
+                "interpreter_path": "python3",
+                "sys_path": get_python_sys_path(),
+        }
+
+    return {}
