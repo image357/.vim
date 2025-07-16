6fcbdc6a9a43cccd85b0ccdb2f9bc0c2547e539f
Marcel Köpke <marcel.koepke@gmail.com>
2025-07-16 14:19:32 +0200

    Update ycm_extra_conf.py
    add include discovery

--- utility/ycm_extra_conf.py
+++ utility/ycm_extra_conf.py
@@ -1,3 +1,36 @@
+import os
+import subprocess
+
+def get_cpp_include_flags():
+    result = subprocess.run([
+        "c++",
+        "-x",
+        "c++",
+        "/dev/null",
+        "-E",
+        "-Wp,-v",
+    ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out = result.stderr.decode("utf-8")
+
+    include_flags = []
+    search_mode = "init"
+    matcher = "#include <...>"
+    for line in out.split("\n"):
+        if search_mode == "collect":
+            path = line.strip()
+            if os.path.isdir(path):
+                include_flags += [
+                        "-isystem",
+                        path,
+                ]
+            else:
+                search_mode = "init"
+        if search_mode == "init":
+            if matcher in line:
+                search_mode = "collect"
+
+    return include_flags
+
 def get_cpp_flags():
     flags = []
 
@@ -9,6 +42,8 @@ def get_cpp_flags():
             "-Wall",
             "-Wextra",
     ]
+
+    flags += get_cpp_include_flags()
 
     return [i for i in flags if i]
 
